a83663d2d3a3574202afa265a54e6c322f6f96e0
ShipudeyHatikvaHolon <49816250+ShipudeyHatikvaHolon@users.noreply.github.com>
2019-05-02 11:38:28 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,49 @@
+
+		<html>
+<head>
+
+	<title>שיפודי התקווה</title>
+
+	
+</head>
+	<body>
+		
+		<footer>
+			<div class="row">
+
+
+
+				<div class="col-12">
+					<div id="footerLinks">
+						<ul>
+							<a href="#">
+								<li>מיקום </li>
+							</a>
+							<a href="#">
+								<li>צור קשר</li>
+							</a>
+
+
+						</ul>
+						<p>מסעדת שיפודי התקווה, הפלד 7 חולון - 03-9453366
+						<p style="font-size: 12px;">&copy כל הזכויות שמורות לעמית בנדט, רובי סומך ועידן זולברג</p>
+					</div>
+					<div class="row">
+					    
+					    <div class="col-sm-12">
+	
+					<a href="https://www.facebook.com/%D7%A9%D7%99%D7%A4%D7%95%D7%93%D7%99-%D7%94%D7%AA%D7%A7%D7%95%D7%95%D7%94-%D7%97%D7%95%D7%9C%D7%95%D7%9F-458821907872310/"><img id="facebookImg" src="images/facebook.jpg" width=100px/>
+</a>
+</div>
+</div>
+				</div>
+
+
+
+
+			</div>
+
+		</footer>
+		
+</body>	
+</html>
---
+++ header.php
@@ -0,0 +1,94 @@
+<?php
+session_start();
+require_once 'configur.php';
+mysqli_set_charset($conn,"utf8");
+?>
+	
+<html>
+
+<head>
+     <meta charset="utf-8">
+	
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<title>שיפודי התקווה</title>
+
+</head>
+
+<body>
+	<header style="font-family: 'Assistant', sans-serif; font-size:14px">
+
+		<div class="row">
+
+
+			<div class="col-xs-12 col-md-3">
+
+<?php 
+						
+				isLoggedIn();
+				
+						function isLoggedIn(){
+							$logged = $_SESSION['user'];
+							$admin = $_SESSION['admin'];
+							
+							if($logged){
+								
+								
+								echo '<span class="right" style="color:#222;">שלום, <span id="name">' . $_SESSION['name'][$id] . '</span> <br> <a href="logout.php "style="color:red;">התנתק</a> </span>';
+								
+							}	
+							
+							else if($admin){
+							    echo "הנך מחובר כמנהל";
+							    echo "<br> <a href='#'>דף ניהול ראשי</a>";
+							    echo '&nbsp &nbsp &nbsp <a href="logout.php" style="color:red;">התנתק</a>';
+							}
+							else{
+								echo "שלום אורח";
+								echo "<br><a href='signin.php'>התחבר</a>";
+							}
+						}
+						
+						
+						
+
+?>
+			
+
+			</div>
+			<div class="col-xs-12 col-md-6">
+
+
+
+				<div id="nav">
+					<ul>
+						<a  href="#" >
+							<li  style="color:orange;">הזמנת שולחן במסעדה</li>
+
+						</a>
+						<a href="#">
+							<li style="color:orange;">משלוחים</li>
+
+						</a>
+						<a href="vouchers.php">
+							<li style="color:orange;">רכישת שוברים</li>
+
+						</a>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-3">
+
+				<div id="logo">
+					<a href="index.html"><img src="images/logo.png" alt="logo" /></a>
+				</div>
+			</div>
+
+
+		</div>
+	</header>
+
+</body>
+
+</html>
