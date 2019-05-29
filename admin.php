dae2e248fecf44426ebb605406b48a61b5a2b334
ShipudeyHatikvaHolon <49816250+ShipudeyHatikvaHolon@users.noreply.github.com>
2019-05-29 16:31:11 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,9 +1,13 @@
 <?php
 require_once 'configur.php';
 session_start();
+
 if($_SESSION['admin'] != true) {
 	header('location: index.php');
 	exit;
+}
+
+require_once 'header_admin.php';
 ?>
 
 
@@ -17,6 +21,7 @@ if($_SESSION['admin'] != true) {
 <head>
 		<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300 | Arimo" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Bellefair" rel="stylesheet">
@@ -101,9 +106,10 @@ a {
 			
 						<div class="col-lg-12" id="buttons">
 						    <center>
-						        				<a class="button button--plain" href="#">יומן המסעדה</a>			
-						<a class="button button--plain" href="#">עמוד דוחות</a>	
-												<a class="button button--plain" href="#">ניהול שולחנות</a>
+														<!-- <a class="button button--plain" href="https://calendar.google.com/calendar/r?tab=mc">יומן המסעדה</a>	 -->
+													<a class="button button--plain" href="https://calendar.google.com/calendar/embed?src=s.hatikva2018%40gmail.com&ctz=Asia%2FJerusalem">יומן המסעדה</a>			 
+						<a class="button button--plain" href="reports&charts3.php">עמוד דוחות</a>	
+												<a class="button button--plain" href="tablesAdmin.php">ניהול שולחנות</a>
 
 						
 			</center>
@@ -115,9 +121,34 @@ a {
 		<div class="container">
 			<center>
 						<div class="col-lg-12" id="buttons">
-						<a class="button button--plain" href="#">ניהול שוברים</a>			
-						<a class="button button--plain" href="#">ניהול חנות</a>					
-						<a class="button button--plain" href="#">הוספת מוצר</a>			
+						<a class="button button--plain" href="vouchers_admin.php">ניהול שוברים</a>			
+						<!-- <a class="button button--plain" href="inventory.php">ניהול חנות</a>					 -->
+						<a class="button button--plain" href="orders_admin.php">ניהול הזמנות משלוחים\שולחנות</a>
+						<!-- <a class="button button--plain" href="upload.php">ניהול מוצרים</a>	 -->
+						<a class="button button--plain" href="upload.php">ניהול מלאי</a>
+						</div>		
+						</center>
+		</div>
+		</div>
+
+		<!-- <div class="row">
+		<div class="container">
+			<center>
+						<div class="col-lg-12" id="buttons">
+							<a class="button button--plain" href="vegetablesAdmin.php">ניהול תוספות</a>			
+							<a class="button button--plain" href="saucesAdmin.php">ניהול רטבים</a>			
+							<a class="button button--plain" href="pitasAdmin.php">ניהול לחם</a>			
+						</div>
+						</center>
+		</div>
+		</div> -->
+		
+		<div class="row">
+		<div class="container">
+			<center>
+						<div class="col-lg-12" id="buttons">
+						<a class="button button--plain" href="contactAdmin.php">ניהול הודעות</a>
+						</div>
 						</center>
 		</div>
 		</div>
@@ -127,6 +158,7 @@ a {
 			<center>
 						<div class="col-lg-12" id="buttons">
 						<a class="button button--plain" href="index.php" style="color:red;">התנתקות</a>	
+						</div>
 						</center>
 		</div>
 		</div>
