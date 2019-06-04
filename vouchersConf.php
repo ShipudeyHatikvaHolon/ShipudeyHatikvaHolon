9983199398109cace93336889ab564ce3e47842a
ShipudeyHatikvaHolon <49816250+ShipudeyHatikvaHolon@users.noreply.github.com>
2019-06-04 15:14:18 +0300

    Update vouchersConf.php

--- vouchersConf.php
+++ vouchersConf.php
@@ -1,8 +1,9 @@
 <?php 
-
+require_once 'configur.php';
 
 include_once 'header.php';
-$user = $_SESSION['user_id'][$id];
+$user = $_SESSION['id'];
+$message = '';
 if (!$user)
 {
      echo '<script language="javascript">';
@@ -15,23 +16,10 @@ if (!$user)
     
     exit;
 } 
-	
-$servername = "localhost:3306";
-$username = "robiso";
-$password = "rakmaccabi";
-$dbname = "robiso_hatikva";
-$message = "";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
- mysqli_set_charset($conn,"utf8"); 
-if ($conn->connect_error) 
-{
-     die("Connection failed: " . $conn->connect_error);
-} 
-$userDetail = $conn->query("SELECT * FROM users WHERE user_id =$user"); 
+$userDetail = mysqli_query($conn ,"SELECT * FROM users WHERE user_id = $user"); 
 	
-		if ($userDetail->num_rows > 0) 
+		if ( mysqli_num_rows($userDetail) > 0) 
 		{
 		  $data = $userDetail->fetch_array();
 		} else
@@ -43,12 +31,15 @@ $userDetail = $conn->query("SELECT * FROM users WHERE user_id =$user");
 <html>
 
 <head>
-
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
 	<link rel="stylesheet" href="vouchers.css" />
 	
 	<script src="vouchers.js"></script>
 	
-
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+		<link href="https://fonts.googleapis.com/css?family=Assistant" rel="stylesheet">
 </head>
 
 <body>
@@ -69,17 +60,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 	
 }
 
-$servername = "localhost:3306";
-$username = "robiso";
-$password = "rakmaccabi";
-$dbname = "robiso_hatikva";
-$message = "";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-     die("Connection failed: " . $conn->connect_error);
-} 
 if($cat==1)
 {
     $addVouchers=15;
@@ -89,71 +70,59 @@ else
      $addVouchers=10;
 }
 
+if(!$cat) {
+  header('location: vouchers.php?errorMessage=חובה לבחור סוג שובר');
+  exit;
+}
 
 
 $sqlFind="SELECT * FROM vouchers WHERE user_id=$user and category_id= $cat";
 
 
-if(mysqli_num_rows(mysqli_query($conn,$sqlFind))==0)
-{
-  $sql1="INSERT INTO vouchers(user_id,category_id,purchase_date,vouchers_left) values ( '$user',' $cat','$date', $addVouchers)";
-	  if ($result=mysqli_query($conn,$sql1))
-     {
-    $message .=  "New record created successfully";
+  if(mysqli_num_rows(mysqli_query($conn,$sqlFind))==0)//new voucher
+  {
+    
+    $sql1="INSERT INTO vouchers(user_id,category_id,purchase_date,vouchers_left) values ( '$user',' $cat','$date', $addVouchers)";
+      if ($result=mysqli_query($conn,$sql1))
+      {
+      $message .=  "New record created successfully";
 
-    } else {
-        $message .=  "Error: " . $sql . "<br>" . $conn->error;
-      echo $message;
+      } else {
+          $message .=  "Error: " . $sql . "<br>" . $conn->error;
+        echo $message;
+        
+      } 
+  }  
+
+  else//activate voucher
+  {
+  
+      if($cat==1)
+  {
+      $addVouchers=15;
+  }
+  else
+  {
+      $addVouchers=10;
+  }
+
+
+    $sql1=" UPDATE vouchers SET vouchers_left= vouchers_left+$addVouchers, purchase_date='$date' WHERE user_id=$user and category_id= $cat ";
+    
+      if ($result=mysqli_query($conn,$sql1))
+      {
+      $message .=  "New record created successfully";
+
+      } else {
+          $message .=  "Error: " . $sql1 . "<br>" . $conn->error;
+        echo $message;
+        
+      } 
       
-    } 
-}  
-
-else
-{
- 
-    if($cat==1)
-{
-    $addVouchers=15;
-}
-else
-{
-     $addVouchers=10;
-}
-
-
-  $sql1=" UPDATE vouchers SET vouchers_left= vouchers_left+$addVouchers, purchase_date='$date' WHERE user_id=$user and category_id= $cat ";
-	  if ($result=mysqli_query($conn,$sql1))
-     {
-    $message .=  "New record created successfully";
-
-    } else {
-        $message .=  "Error: " . $sql1 . "<br>" . $conn->error;
-      echo $message;
       
-    } 
-    
-    
-}
-
-mysqli_set_charset($conn,"utf8");
-mysqli_query($conn,"set character_set_client='utf8'");
-mysqli_query($conn,"set character_set_results='utf8'");
-mysqli_query($conn ,"set collation_connection='utf8'");
-mysqli_set_charset($conn,"utf8");
-
-$sql = "SELECT * FROM category where id_category=$cat";
-	$result = mysqli_query($conn, $sql);
-	
-	 while ($res=mysqli_fetch_array($result))
-	{
-	    $catName=$res['name'];
-	    $catDescription=$res['description'];
-	    $catPrice=$res['price'];
-	     
-	}
+  }
 
 
-    
 ?>	
 	
 <?php require 'footer.php'?>
